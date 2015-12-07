a57af10886df491b666ad579766e6c73951aa3db
Josh Juster <juster@gmail.com>
2015-12-07 14:25:47 -0500

    no www

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Atidbit
   class Application < Rails::Application
+
+    if Rails.env.production?
+      config.middleware.insert_before Rack::Lock, Rack::NoWWW
+    end
+        
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
