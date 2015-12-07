d7dab8ca301a215b510dd04560c34faef702774e
Josh Juster <juster@gmail.com>
2015-12-07 10:54:00 -0500

    first commit

---
+++ app/controllers/redirect_controller.rb
@@ -0,0 +1,33 @@
+class RedirectController < ApplicationController 
+  def index
+    short_slug = params[:hex]
+    klass = get_class(short_slug[0])
+    id = convert_hex_id(short_slug.slice(1, short_slug.length))
+    redirect_to "https://www.wildvillage.com/tidbits/#{klass}/#{id}"
+  end
+
+  private
+
+  def convert_hex_id(hex)
+    hex.to_i(16)
+  end
+
+  def get_class(identifier)
+    case identifier
+    when "f"
+      "facts"
+    when "j"
+      "jokes"
+    when "e"
+      "explanations"
+    when "q"
+      "quotes"
+    when "h"
+      "thoughts"
+    when "t"
+      "tips"
+    else
+      nil
+    end    
+  end
+end
